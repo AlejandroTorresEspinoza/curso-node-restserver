4b0ae5eebcfda67d0d63ad5cea9ee6b300cba47b
Alejandro <92492385+AlejandroTorresEspinoza@users.noreply.github.com>
2023-06-21 22:05:20 -0500

    Update server.js

--- models/server.js
+++ models/server.js
@@ -263,15 +263,15 @@ const express = require('express');
 const cors = require('cors');
 class Server {
         constructor() {
-                this.app = express();
-                this.port = process.env.PORT;
-                this.usuariosPath = '/api/usuarios';
-                //Middelware
-                this.middlewares();
-                //Rutas de la aplicacion
-                this.routes();
-               
-            }
+ this.app = express();
+ this.port = process.env.PORT || 3000;
+ this.usuariosPath = '/api/usuarios';
+ //Middelware
+ this.middlewares();
+ //Rutas de la aplicacion
+ this.routes();
+
+ }
    
         middlewares(){
             //CORS
