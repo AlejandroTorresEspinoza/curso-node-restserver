2c7f3839078d6dc6bedf30ea6640e56e1464246e
Alejandro <alejandro.torres4@unmsm.edu.pe>
2023-06-29 15:07:53 -0500

    Fin Seccion 9

--- models/server.js
+++ models/server.js
@@ -1,296 +1,39 @@
-//Ejemplo 1
-/*const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../database/config');
 class Server {
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-        this.routes();
-    }
-    routes() {
-        this.app.get('/',  (req, res) => {
-            res.send('Hello World');
-          })
-    }
+    constructor() {
+                this.app = express();
+                this.port = process.env.PORT;
+                this.usuariosPath = '/api/usuarios';
+                //llamada a conectarDB
+                this.conectarDB();
+                //Middelware
+                this.middlewares();
+                //Rutas de la aplicacion
+                this.routes();
+               
+            }
+            async conectarDB() {
+                await dbConnection();
+            }
+           
+            //aque se define el metodo middleware que publicara la carpeta public
+            middlewares(){
+                    //CORS
+                    this.app.use(cors());
+                    //lectura y parseo del body recibe lo que se envia
+                    this.app.use(express.json());
+                    //directorio publico
+                    this.app.use(express.static('public'));
+                }
+        routes() {
+                    this.app.use(this.usuariosPath, require('../routes/usuarios'))
+                }
     listen() {
         this.app.listen(this.port, () =>{
             console.log('Servidor corriendo en puerto ',this.port)
         })
     }
-}
-module.exports = Server;*/
-
-//Ejemplo 2
-/*const express = require('express')
-class Server {
-constructor() {
-            this.app = express();
-            this.port = process.env.PORT;
-            //Middelware
-            this.middlewares();
-            //Rutas de la aplicacion
-            this.routes();
-        }
-       
-        //aque se define el metodo middleware que publicara la carpeta public
-        middlewares(){
-            this.app.use(express.static('public'));
-        }
-
-    routes() {
-                this.app.get('/',  (req, res) => {
-                    res.send('Hello World');
-                  })
-            }
-            listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-}
-module.exports = Server;*/
-
-//Ejemplo 3
-/*const express = require('express')
-class Server {
-constructor() {
-            this.app = express();
-            this.port = process.env.PORT;
-            //Middelware
-            this.middlewares();
-            //Rutas de la aplicacion
-            this.routes();
-        }
-       
-        //aque se define el metodo middleware que publicara la carpeta public
-        middlewares(){
-            this.app.use(express.static('public'));
-        }
-
-    routes() {
-                this.app.get('/api',  (req, res) => {
-                    res.send('Hello World');
-                  })
-            }
-            listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-}
-module.exports = Server;*/
-
-//Ejemplo 4
-/*const express = require('express')
-class Server {
-constructor() {
-            this.app = express();
-            this.port = process.env.PORT;
-            //Middelware
-            this.middlewares();
-            //Rutas de la aplicacion
-            this.routes();
-        }
-       
-        //aque se define el metodo middleware que publicara la carpeta public
-        middlewares(){
-            this.app.use(express.static('public'));
-        }
-
-    routes() {
-                this.app.get('/api',  (req, res) => {
-                    res.json('Hello World');
-                  })
-            }
-            listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-}
-module.exports = Server;*/
-
-//Ejemplo 5
-/*const express = require('express')
-class Server {
-constructor() {
-            this.app = express();
-            this.port = process.env.PORT;
-            //Middelware
-            this.middlewares();
-            //Rutas de la aplicacion
-            this.routes();
-        }
-       
-        //aque se define el metodo middleware que publicara la carpeta public
-        middlewares(){
-            this.app.use(express.static('public'));
-        }
-
-    routes() {
-                this.app.get('/api',  (req, res) => {
-                    res.json({
-                        ok: true,
-                        msg: 'get API'
-                    });
-                  })
-            }
-            listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-}
-module.exports = Server;*/
-
-//Ejemplo 6
-/*const express = require('express')
-class Server {
-constructor() {
-            this.app = express();
-            this.port = process.env.PORT;
-            //Middelware
-            this.middlewares();
-            //Rutas de la aplicacion
-            this.routes();
-        }
-       
-        //aque se define el metodo middleware que publicara la carpeta public
-        middlewares(){
-            this.app.use(express.static('public'));
-        }
-
-    routes() {
-                this.app.get('/api',  (req, res) => {
-                    res.json({
-                        msg: 'get API'
-                    });
-                })
-                this.app.put('/api',  (req, res) => {
-                    res.json({
-                        msg: 'put API'
-                    });
-                });
-        
-                this.app.post('/api',  (req, res) => {
-                    res.json({
-                        msg: 'post API'
-                    });
-                });
-                this.app.delete('/api',  (req, res) => {
-                    res.json({
-                        msg: 'delete API'
-                    });
-                });
-            }
-            listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-}
-module.exports = Server;*/
-
-//Ejemplo 7
-/*const express = require('express');
-const cors = require('cors');
-class Server {
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
-        //Middelware
-        this.middlewares();
-        //Rutas de la aplicacion
-        this.routes();
-    }
-   
-    //aque se define el metodo middleware que publicara la carpeta public
-    middlewares(){
-        //CORS
-        this.app.use(cors());
-        //directorio publico
-        this.app.use(express.static('public'));
-    }
-routes() {
-            this.app.use('/api/usuarios', require('../router/user'))
-        }
-
-    listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-
-}
-module.exports = Server;*/
-
-//Ejemplo 8
-/*const express = require('express');
-const cors = require('cors');
-class Server {
-        constructor() {
-                this.app = express();
-                this.port = process.env.PORT;
-                this.usuariosPath = '/api/usuarios';
-                //Middelware
-                this.middlewares();
-                //Rutas de la aplicacion
-                this.routes();
-               
-            }
-   
-    //aque se define el metodo middleware que publicara la carpeta public
-    middlewares(){
-        //CORS
-        this.app.use(cors());
-        //directorio publico
-        this.app.use(express.static('public'));
-    }
-routes() {
-            this.app.use(this.usuariosPath, require('../routes/usuarios'))
-        }
-
-    listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-
-}
-module.exports = Server;*/
-
-//Ejemplo 9
-const express = require('express');
-const cors = require('cors');
-class Server {
-        constructor() {
-                this.app = express();
-                this.port = process.env.PORT;
-                this.usuariosPath = '/api/usuarios';
-                //Middelware
-                this.middlewares();
-                //Rutas de la aplicacion
-                this.routes();
-               
-            }
-   
-        middlewares(){
-            //CORS
-            this.app.use(cors());
-            //lectura y parseo del body recibe lo que se envia
-            this.app.use(express.json());
-            //directorio publico
-            this.app.use(express.static('public'));
-            }
-    
-routes() {
-            this.app.use(this.usuariosPath, require('../routes/usuarios'))
-        }
-
-    listen() {
-                this.app.listen(this.port, () =>{
-                    console.log('Servidor corriendo en puerto ',this.port)
-                })
-            }
-
 }
 module.exports = Server;
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -35,13 +35,136 @@ router.patch('/', (req, res) => {
 module.exports = router;*/
 
 //Ejemplo 2
-const {Router} = require('express');
+/*const {Router} = require('express');
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } =
 require('../controller/usuarios');
 const router = Router();
-router.get('/', usuariosGet );
+router.get('/', usuariosGet )
+//router.put('/', usuariosPut );
 router.put('/:id', usuariosPut );
 router.post('/',usuariosPost );
 router.delete('/',usuariosDelete  );
+router.patch('/', usuariosPatch );
+module.exports = router;*/
+
+//Ejemplo 3
+/*const {Router} = require('express');
+const router = Router();
+router.get('/',  (req, res) => {
+    res.json({
+        msg: 'get API'
+    });
+})
+router.put('/',  (req, res) => {
+    res.json({
+        msg: 'put API'
+    });
+});
+router.post('/',  (req, res) => {
+    res.json({
+        msg: 'post API'
+
+    });
+});
+router.delete('/',  (req, res) => {
+    res.json({
+        msg: 'delete API'
+    });
+});
+router.patch('/',  (req, res) => {
+    res.json({
+        msg: 'patch API'
+    });
+});
+module.exports = router;
+*/
+
+
+
+//14fin
+/*const {Router} = require('express');
+const { check } = require('express-validator');
+
+const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controller/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
+const router = Router();
+router.get('/', usuariosGet )
+//router.put('/', usuariosPut );
+router.put('/:id', usuariosPut );
+//router.post('/',usuariosPost );
+router.post('/',[
+    check('nombre','El Nombre es obligatorio').not().isEmpty(),
+    check('correo','El correo no es valido').isEmail(),
+    check('password','El Password obligatorio y mas de 6 letras').isLength({min: 6}),
+    check('rol','No es un Rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    validarCampos
+    ],usuariosPost );     
+router.delete('/',usuariosDelete  );
+router.patch('/', usuariosPatch );
+module.exports = router;*/
+
+//15fin
+/*const {Router} = require('express');
+const { check } = require('express-validator');
+const Role = require('../models/role');
+
+const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controller/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
+const router = Router();
+router.get('/', usuariosGet )
+//router.put('/', usuariosPut );
+router.put('/:id', usuariosPut );
+//router.post('/',usuariosPost );
+router.post('/',[
+    check('nombre','El Nombre es obligatorio').not().isEmpty(),
+    check('correo','El correo no es valido').isEmail(),
+    check('password','El Password obligatorio y mas de 6 letras').isLength({min: 6}),
+    // check('rol','No es un Rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom(async(rol = '') => {
+        const existeRol = await Role.findOne({rol});
+        if(!existeRol) {
+            throw new Error(`El rol ${rol} no esta registrado en la DB`)
+        }
+    }),
+    validarCampos
+    ],usuariosPost );   
+router.delete('/',usuariosDelete  );
+router.patch('/', usuariosPatch );
+module.exports = router;*/
+
+//16fin
+const {Router} = require('express');
+const { check } = require('express-validator');
+const Role = require('../models/role');
+const { esRoleValido, existeUsuarioPorId } = require('../helpers/db-validators');
+
+const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controller/usuarios');
+const { validarCampos } = require('../middlewares/validar-campos');
+const router = Router();
+router.get('/', usuariosGet )
+//router.put('/', usuariosPut );
+router.put('/:id',[
+    check('id','No es un ID valido ').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    validarCampos
+    ], usuariosPut );
+
+//router.post('/',usuariosPost );
+router.post('/',[
+    check('nombre','El Nombre es obligatorio').not().isEmpty(),
+    check('correo','El correo no es valido').isEmail(),
+    check('password','El Password obligatorio y mas de 6 letras').isLength({min: 6}),
+    // check('rol','No es un Rol valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom(esRoleValido),
+    validarCampos
+    ],usuariosPost );
+       
+    router.delete('/:id',[
+        check('id','No es un ID valido ').isMongoId(),
+        check('id').custom(existeUsuarioPorId),
+        validarCampos
+        ],usuariosDelete );
+        
 router.patch('/', usuariosPatch );
 module.exports = router;
